1daa443485824a23376f7888a734a1ef5bbd585b
0xngmi <0xngmi@protonmail.com>
2023-09-08 20:02:40 +0100

    deployment info on eth

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const LIB: {
   };
 } = {
   [mainnet.id]: {
-    claimContract: "0xf33197CA64f66A36a2Ac9D02e3b954455F971761",
+    claimContract: "0xBDF31bd6aC19C964043801186b12BDa46A2e0430",
     usdcContract: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     proofsFilename: "ethereum-proofs.json",
   },
